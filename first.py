ad9517f208a14c9222c7a1d98403018dd5250511
fade437 <samyogbohara437@gmail.com>
2021-02-18 09:27:32 +0545

    message

---
+++ first.py
@@ -0,0 +1,4 @@
+l=int(input('Enter length '))
+b=int(input('Enter breadth '))
+p= 2 * (l+b)
+print('The perimeter of rectangle is ',p)
---
+++ second.py
@@ -0,0 +1,4 @@
+a=True+10
+print('a:',a)
+b=False+12
+print('b:',b)
